236a8c0e55e1cb0bf7ab32185722b2d21d35900c
mac_itthikon <zero-no-connect@hotmail.com>
2025-10-12 22:37:59 +0700

    first web React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+const name="ชาลี"
+const address="กรุงเทพมหานคร"
+  return (
+  <>    
+    <h1>สวัสดีครับคุณ {name}</h1>
+    <h2>ที่อยู่ : {address}</h2>
+  </>
+  )
+}
+
+export default App;
