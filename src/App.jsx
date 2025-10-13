7fb38aa263d727b6da04dff622974d5f856b6e5b
mac_itthikon <zero-no-connect@hotmail.com>
2025-10-13 07:31:11 +0700

    add function, add button

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ function App() {
 const name = "ชาลี"
 const address = "กรุงเทพมหานคร"
 const [age,setAge]=useState(40)
+
+function add(){
+
+}
+function subtract(){
+
+}
   return (
   <>    
     <h1>สวัสดีครับคุณ {name}</h1>
     <h2>ที่อยู่ : {address}</h2>
     <h3>อายุ : {age} ปี</h3>
-    <button>เพิ่ม</button>
-    <button>ลด</button>
+    <button onClick={add}>เพิ่ม</button>
+    <button onClick={subtract}>ลด</button>
   </>
   )
 }
