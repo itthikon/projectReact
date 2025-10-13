63098135a7d787e55a4892faee98daa9d2c11857
mac_itthikon <zero-no-connect@hotmail.com>
2025-10-13 07:51:37 +0700

    remove excess arrowfunction

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,14 @@ const name = "ชาลี"
 const address = "กรุงเทพมหานคร"
 const [age,setAge]=useState(40)
 
-function add(){
-  setAge(age + 1);
-}
-function subtract(){
-  setAge(age - 1);
-}
-function reset(){
-  setAge(40);
-}
 
   return (
   <>    
     <h1>สวัสดีครับคุณ {name}</h1>
     <h2>ที่อยู่ : {address}</h2>
     <h3>อายุ : {age} ปี</h3>
-    <button onClick={()=>setAge(age + 1)</>}>เพิ่ม</button>
-    <button onClick={()=>setAge(age - 1)</button>}>ลด</button>
+    <button onClick={()=>setAge(age + 1)}>เพิ่ม</button>
+    <button onClick={()=>setAge(age - 1)}>ลด</button>
     <button onClick={()=>setAge(40)}>เคลียร์</button>
   </>
   )
