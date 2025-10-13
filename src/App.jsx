393b5cb7392ee3de07223d50c316b02c00baf96f
mac_itthikon <zero-no-connect@hotmail.com>
2025-10-13 07:25:51 +0700

    change App.jsx useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+
+
 function App() {
-const name="ชาลี"
-const address="กรุงเทพมหานคร"
+const name = "ชาลี"
+const address = "กรุงเทพมหานคร"
+const [age,setAge]=useState(40)
   return (
   <>    
     <h1>สวัสดีครับคุณ {name}</h1>
     <h2>ที่อยู่ : {address}</h2>
+    <h3>อายุ : {age} ปี</h3>
+    <button>เพิ่ม</button>
+    <button>ลด</button>
   </>
   )
 }
