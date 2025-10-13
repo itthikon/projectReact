b89a9cf7ca08ee4ddd3e32a2677afceed5bd98a9
mac_itthikon <zero-no-connect@hotmail.com>
2025-10-13 08:36:13 +0700

    add useState & show in console

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 
 function App() {
-
-
+  const [data,setdata]=useState([
+    {id:1,name:"อิทธิกร",gender:"ชาย"},
+    {id:2,name:"สิริมา",gender:"หญิง"},
+    {id:3,name:"ยอด",gender:"ชาย"}
+  ])
+console.table(data)
 
   return (
   <>    
